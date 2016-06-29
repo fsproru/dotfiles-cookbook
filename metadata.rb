bc204e726efdafc64a3c72107dcd525ad995d3ec
Alex Tamoykin <alex@rigthbalance.io>
2016-06-29 16:04:28 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'dotfiles-cookbook'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures dotfiles-cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
