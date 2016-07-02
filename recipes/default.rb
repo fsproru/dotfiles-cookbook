cec00aeb95ccd9f6ed3de81b1f508da265725336
Alex Tamoykin <alex@rigthbalance.io>
2016-07-01 17:43:12 -0700

    Clone and install dotfiles

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,25 @@
 # Cookbook Name:: dotfiles-cookbook
 # Recipe:: default
 #
-# Copyright 2016, YOUR_COMPANY_NAME
+# Copyright 2016, The Right Balance
 #
 # All rights reserved - Do Not Redistribute
 #
+
+home_path = File.expand_path('~/')
+dotfiles_location = "#{home_path}/.dotfiles"
+dotfiles_user = 'at'
+dotfiles_group = 'staff'
+
+git dotfiles_location do
+  repository 'git://github.com/fsproru/dotfiles.git'
+  action :checkout
+  user dotfiles_user
+  group dotfiles_group
+end
+
+execute './install.sh' do
+  user dotfiles_user
+  group dotfiles_group
+  cwd dotfiles_location
+end
